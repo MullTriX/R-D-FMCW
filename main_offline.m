f594c505feba3b9ebb157072192f0cab6452f851
Mateusz Kaźmierczak <mulltrixpl@gmail.com>
2026-03-23 10:56:53 +0100

    feat: include 4 receiver anthenas in offline processing

--- main_offline.m
+++ main_offline.m
@@ -1,91 +1,143 @@
-%% INICJALIZACJA I KONFIGURACJA
+%% 1. INICJALIZACJA I WCZYTANIE PARAMETRÓW
+clear all; close all;
+
 recordLocation = 'Dataset/10s_nic_plus_machanie/';
 filePath = fullfile(recordLocation, 'iqData_RecordingParameters.mat');
 if ~isfile(filePath), error('Nie znaleziono pliku: %s', filePath); end
 
 temp = load(filePath);
 dcaRecordingParams = temp.RecordingParameters;
+
+% Parametry fizyczne
+c = 3e8; 
 fs = dcaRecordingParams.ADCSampleRate * 1e3;
-sweepSlope = dcaRecordingParams.SweepSlope * 1e12; 
-nr = dcaRecordingParams.SamplesPerChirp;
 fc = dcaRecordingParams.CenterFrequency * 1e9;
 tpulse = 2 * dcaRecordingParams.ChirpCycleTime * 1e-6;
-prf = 1 / tpulse;
+sweepslope = dcaRecordingParams.SweepSlope * 1e12; 
+nr = dcaRecordingParams.SamplesPerChirp;
+lambda = c / fc;
+nchirp = dcaRecordingParams.NumChirps;
 
-% Obiekty przetwarzające
-rangeresp = phased.RangeResponse('RangeMethod', 'FFT', 'RangeFFTLengthSource', 'Property', 'RangeFFTLength', nr, 'SampleRate', fs, 'SweepSlope', sweepSlope, 'ReferenceRangeCentered', false);
-rd_response = phased.RangeDopplerResponse('SweepSlope', sweepSlope, 'SampleRate', fs, 'DopplerOutput', 'Speed', 'OperatingFrequency', fc, 'PRFSource', 'Property', 'PRF', prf, 'RangeMethod', 'FFT', 'RangeFFTLengthSource', 'Property', 'RangeFFTLength', nr, 'ReferenceRangeCentered', false);
+%% 2. GENEROWANIE OSI (Zasięg, Prędkość i Kąt)
+% Oś Zasięgu (odcięcie na 5 metrach)
+rangeAxis = (0:nr-1) * (c * fs) / (2 * sweepslope * nr);
+idx5m = find(rangeAxis <= 5, 1, 'last');
+rangeAxis5m = rangeAxis(1:idx5m);
 
-% Inicjalizacja CFAR
+% Oś Prędkości
+v_max = lambda / (4 * tpulse);
+numChirps = nchirp / 2; % Bierzemy tylko nadajnik TX1 (co drugi chirp)
+v_grid = linspace(-v_max, v_max, numChirps);
+
+% Oś Kąta
+numAngleBins = 64;
+k_vec = -numAngleBins/2 : numAngleBins/2-1;
+sin_theta = 2 * k_vec / numAngleBins;
+sin_theta(sin_theta > 1) = 1; sin_theta(sin_theta < -1) = -1; 
+theta_axis_deg = asind(sin_theta);
+
+%% 3. INICJALIZACJA CFAR 2D
 guard_size = [2 2]; 
 train_size = [4 4]; 
-cfar2D = phased.CFARDetector2D('GuardBandSize', guard_size, 'TrainingBandSize', train_size, 'ProbabilityFalseAlarm', 1e-5, 'Method', 'CA');
+cfar2D = phased.CFARDetector2D('GuardBandSize', guard_size, 'TrainingBandSize', train_size, ...
+    'ProbabilityFalseAlarm', 1e-5, 'Method', 'CA');
 
-%% PRZYGOTOWANIE GUI
-fr = dca1000FileReader('RecordLocation', recordLocation);
-iqData = read(fr, 1);
-[~, r_grid_1d] = rangeresp(squeeze(iqData{1}(:,1,:)));
-[~, r_grid_2d, v_grid] = rd_response(squeeze(iqData{1}(:,1,1:2:end)));
-
-half_idx = floor(length(r_grid_2d) / 2);
-r_grid_half = r_grid_2d(1:half_idx);
-
-% Tworzenie siatki dla CFAR
+% Siatka CFAR
 row_start = train_size(1) + guard_size(1) + 1;
-row_end   = half_idx - (train_size(1) + guard_size(1));
+row_end   = idx5m - (train_size(1) + guard_size(1));
 col_start = train_size(2) + guard_size(2) + 1;
-col_end   = length(v_grid) - (train_size(2) + guard_size(2));
+col_end   = numChirps - (train_size(2) + guard_size(2));
 [c_grid, r_grid_cfar] = meshgrid(col_start:col_end, row_start:row_end);
 CUTIdx = [r_grid_cfar(:) c_grid(:)]';
 
-% Rysowanie okien
-figure('Name', 'Analiza Offline - Modułowa', 'Position', [100, 100, 1200, 500]);
-subplot(1, 2, 1);
-h_plot = plot(r_grid_half, zeros(half_idx, 1), 'b', 'LineWidth', 1.5);
-grid on; title('Profil Odległości'); xlabel('Odległość [m]'); ylabel('Moc [dB]'); ylim([40 120]); 
+%% 4. PRZYGOTOWANIE INTERFEJSU (GUI)
+fig = figure('Name', 'Analiza Offline: CFAR & Kąt', 'Position', [100, 100, 1200, 500]);
 
-subplot(1, 2, 2);
-h_img = imagesc(v_grid, r_grid_half, zeros(half_idx, length(v_grid)));
-axis xy; colormap('jet'); colorbar; title('Mapa Range-Doppler'); xlabel('Prędkość [m/s]'); ylabel('Odległość [m]'); clim([50 120]); 
-hold on; h_target = plot(0, 0, 'ro', 'MarkerSize', 15, 'LineWidth', 3, 'Visible', 'off'); hold off;
+% Panel 1: Mapa Range-Doppler
+axMap = subplot(1, 2, 1);
+hImage = imagesc(axMap, 'XData', v_grid, 'YData', rangeAxis5m, 'CData', zeros(idx5m, numChirps));
+set(axMap, 'YDir', 'normal');
+xlabel(axMap, 'Prędkość (m/s)'); ylabel(axMap, 'Odległość (m)');
+title(axMap, 'Mapa Range-Doppler');
+colormap(axMap, 'jet'); colorbar(axMap);
+clim(axMap, [50 110]); % Zablokowanie skali kolorów (dostosuj jeśli trzeba)
 
-%% PĘTLA GŁÓWNA
+% Panel 2: Wykres Polarny (Kąt)
+axPolar = subplot(1, 2, 2); 
+hTarget = polarplot(0, 0, 'ro', 'MarkerSize', 15, 'MarkerFaceColor', 'r', 'LineWidth', 2); 
+axPolar = gca; 
+axPolar.ThetaZeroLocation = 'top'; 
+axPolar.ThetaDir = 'clockwise';
+axPolar.ThetaLim = [-45 45]; 
+axPolar.RLim = [0 5];        
+title(axPolar, 'Lokalizacja Ruchu (\pm45^\circ)');
+
+%% 5. GŁÓWNA PĘTLA PRZETWARZANIA OFFLINE
 disp('Odtwarzanie pliku rozpoczęte...');
-fr = dca1000FileReader('RecordLocation', recordLocation); 
+fr = dca1000FileReader('RecordLocation', recordLocation);
 total_frames = fr.NumDataCubes;
+
+% Okna wygładzające dla FFT
+winRange = hamming(nr);
+winDoppler = reshape(hamming(numChirps), 1, 1, numChirps);
 
 while (fr.CurrentPosition <= total_frames)
     current_frame = fr.CurrentPosition;
-    iqData = read(fr, 1); iqData = iqData{1};
     
-    % 1. Przetwarzanie Range (Lewy wykres)
-    iqDataRx1 = squeeze(iqData(:,1,:)) - mean(squeeze(iqData(:,1,:)), 1); 
-    resp_range = rangeresp(iqDataRx1);
-    resp_range_db = 20 * log10(abs(resp_range(1:half_idx, 1)) + 1e-6);
+    % --- A. Wczytanie ramki danych ---
+    iqDataRaw = read(fr, 1);
+    iqDataRaw = iqDataRaw{1};
     
-    % 2. Przetwarzanie Range-Doppler z MTI
-    iqDataRx1Tx1 = squeeze(iqData(:,1,1:2:end));
-    iqData_mti = MTI(iqDataRx1Tx1); % <-- WYWOŁANIE FUNKCJI MTI
+    % Wyciągamy WSZYSTKIE 4 anteny dla nadajnika TX1
+    iqData4RX = squeeze(iqDataRaw(:, 1:4, 1:2:end)); 
     
-    resp_rd = rd_response(iqData_mti);
-    resp_rd_half = resp_rd(1:half_idx, :);
-    resp_rd_db = 20 * log10(abs(resp_rd_half) + 1e-6);
+    % --- B. Filtry MTI (Zewnętrzna funkcja) ---
+    iqData4RX_mti = MTI(iqData4RX);
     
-    % 3. Detekcja CFAR
-    power_matrix = abs(resp_rd_half).^2;
-    [found, tgt_r, tgt_v] = CFAR(power_matrix, cfar2D, CUTIdx, r_grid_half, v_grid); % <-- WYWOŁANIE FUNKCJI CFAR
+    % --- C. FFT Odległości i Prędkości ---
+    % Range FFT
+    iqData4RX_win = iqData4RX_mti .* winRange; 
+    rangeFFT = fft(iqData4RX_win, nr, 1);
+    rangeFFT_5m = rangeFFT(1:idx5m, :, :);
     
-    % 4. Aktualizacja GUI
-    set(h_plot, 'YData', resp_range_db);
-    set(h_img, 'CData', resp_rd_db);
+    % Doppler FFT
+    dopplerFFT_4RX = fftshift(fft(rangeFFT_5m .* winDoppler, numChirps, 3), 3);
     
-    if found
-        set(h_target, 'XData', tgt_v, 'YData', tgt_r, 'Visible', 'on');
+    % --- D. Mapa dla Anteny 1 ---
+    dopplerFFT_RX1 = squeeze(dopplerFFT_4RX(:, 1, :));
+    powerDB = 20 * log10(abs(dopplerFFT_RX1) + 1e-6);
+    
+    % --- E. Detekcja CFAR (Zewnętrzna funkcja) ---
+    power_linear = abs(dopplerFFT_RX1).^2;
+    % Szukamy do 2.5m, minimalna moc 85 dB (Dostrój te wartości!)
+    [found, tgt_r, tgt_v, r_idx, v_idx] = CFAR(power_linear, cfar2D, CUTIdx, rangeAxis5m, v_grid, 2.5, 85);
+    
+    % --- F. Obliczenie Kąta (Tylko jeśli CFAR coś znalazł) ---
+    if found && tgt_r > 0.1
+        % Wyciągamy dane z 4 anten dla konkretnego piku wyznaczonego przez CFAR
+        spatialData = squeeze(dopplerFFT_4RX(r_idx, :, v_idx)); 
+        
+        % Angle FFT
+        angleFFT = fftshift(fft(spatialData, numAngleBins));
+        [~, maxAngleIdx] = max(abs(angleFFT));
+        targetAngleDeg = theta_axis_deg(maxAngleIdx);
+        
+        % Aktualizacja kropki
+        if targetAngleDeg >= -45 && targetAngleDeg <= 45
+            set(hTarget, 'ThetaData', deg2rad(targetAngleDeg), 'RData', tgt_r, 'Visible', 'on');
+        else
+            set(hTarget, 'Visible', 'off'); 
+        end
     else
-        set(h_target, 'Visible', 'off');
+        set(hTarget, 'Visible', 'off'); 
     end
     
-    title(subplot(1,2,2), sprintf('Mapa Range-Doppler | Klatka: %d/%d', current_frame, total_frames));
-    drawnow; pause(0.05); 
+    % --- G. Aktualizacja Ekranu ---
+    set(hImage, 'CData', powerDB);
+    title(subplot(1,2,2), sprintf('Lokalizacja Ruchu | Klatka: %d/%d', current_frame, total_frames));
+    
+    drawnow;
+    pause(0.05); % Spowolnienie do ~20 FPS dla płynnego wideo
 end
+
 disp('Koniec odtwarzania.');
